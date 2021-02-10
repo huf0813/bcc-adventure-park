38f1b13926977c6b3ee558cd2483776f31c82b49
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-10 11:56:27 +0700

    Add tests for get park by id endpoint

--- test/park.js
+++ test/park.js
@@ -6,29 +6,37 @@ chai.use(chaiHttp)
 const db = require('../src/utils/db')
 
 function assertIsValidFullParkObject(park){
-  it('should be a valid park object', function(done){
-    assert.isObject(park, "park is not an object")
-    assert.hasAllKeys(park, ["id", "name", "details", "entranceFee"], "park object doesn't have the required keys")
-    assert.typeOf(park.id, "number", "id is not a number")
-    assert.typeOf(park.name, "string", "name is not a string")
-    assert.typeOf(park.details, "string", "details is not a string")
-    assert.typeOf(park.entranceFee, "number", "entranceFee is not a number")
-    done()
-  })
+  assert.isObject(park, "park is not an object")
+  assert.hasAllKeys(park, ["id", "name", "details", "entranceFee"], "park object doesn't have the required keys")
+  assert.typeOf(park.id, "number", "id is not a number")
+  assert.typeOf(park.name, "string", "name is not a string")
+  assert.typeOf(park.details, "string", "details is not a string")
+  assert.typeOf(park.entranceFee, "number", "entranceFee is not a number")
+}
+
+const dummyPark = { name: "Integration Testing Park #1", details: "Integration Testing Park #1", entranceFee: 420360 }
+function assertDummyPark(park){
+  assertIsValidFullParkObject(park)
+  assert.isNumber(park.id, "id should be generated and be a number")
+  assert.equal(park.name, "Integration Testing Park #1", "name is not the same as provided")
+  assert.equal(park.details, "Integration Testing Park #1", "details is not the same as provided")
+  assert.equal(park.entranceFee, 420360, "entranceFee is not the same as provided")
 }
 
 describe("/park endpoint", function () {
+  before(function(done) {
+    db('parks').delete().then(() => done())
+  })
+
   describe('POST /park, insert a new park with full information', function(){
     before(function(done){
-      db('parks').delete().then(() => {
-        this.requester.post('/park')
-          .send({ name: "Integration Testing Park #1", details: "Integration Testing Park #1", entranceFee: 420360 })
-          .end((err, res) => {
-            if(err){ console.error(err) }
-            this.requestResult = res
-            done()
-          })
-      })
+      this.requester.post('/park')
+        .send(dummyPark)
+        .then((res, err) => {
+          if(err){ console.error(err) }
+          this.requestResult = res
+          done()
+        })
     })
 
     it('should return status code 200', function(done){
@@ -37,12 +45,7 @@ describe("/park endpoint", function () {
     })
 
     it('should return a full park object with the information provided', function(done){
-      const park = this.requestResult.body
-      assertIsValidFullParkObject(park)
-      assert.isNumber(park.id, "id should be generated and be a number")
-      assert.equal(park.name, "Integration Testing Park #1", "name is not the same as provided")
-      assert.equal(park.details, "Integration Testing Park #1", "details is not the same as provided")
-      assert.equal(park.entranceFee, 420360, "entranceFee is not the same as provided")
+      assertDummyPark(this.requestResult.body)
       done()
     })
   })
@@ -50,7 +53,7 @@ describe("/park endpoint", function () {
   describe('GET /park, get list of parks', function(){
     before(function(done){
       this.requester.get('/park')
-        .end((err, res) => {
+        .then((res, err) => {
           if(err){ console.error(err) }
           this.requestResult = res
           done()    
@@ -73,6 +76,27 @@ describe("/park endpoint", function () {
       if(this.requestResult.body.parks.length != 0){
         this.requestResult.body.parks.forEach(park => assertIsValidFullParkObject(park))
       }
+      done()
+    })
+  })
+  
+  describe('GET /park/:id, get an existing park with an id', function(){
+    before(async function(){
+      await this.requester.post("/park").send(dummyPark).then((res, err) => {
+        this.idToBeTested = res.body.id
+      })
+      await this.requester.get("/park/" + this.idToBeTested).then((res, err) => {
+        this.requestResult = res
+      })
+    })
+
+    it('should return status code 200', function(done){
+      assert.equal(this.requestResult.status, 200)
+      done()
+    })
+
+    it('should return a full park object with dummy park information', function(done){
+      assertDummyPark(assertDummyPark(this.requestResult.body))
       done()
     })
   })
