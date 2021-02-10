f53607c711544569002c76c82555c9d8fa8411c3
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-10 20:42:54 +0700

    Add tests for malformed user register requests

--- test/auth.js
+++ test/auth.js
@@ -9,27 +9,59 @@ const newUser = { email: "izf@izfaruqi.com", pass: "Tr0ub4dor&3" }
 
 describe('/auth endpoints', function(){
   describe('POST /auth/register, for registering new users', function(){
-    before(async function(){
-      await this.requester.post('/auth/register').send({ email: newUser.email, pass: newUser.pass }).then((res, err) => {
-        this.requestResult = res
+    describe('insert a new user with an email and a password', function(){
+      before(async function(){
+        await db('users').delete()
+        await this.requester.post('/auth/register').send({ email: newUser.email, pass: newUser.pass }).then((res, err) => {
+          this.requestResult = res
+        })
+      })
+  
+      it('should return status code 200', function(done){
+        assert.equal(this.requestResult.status, 200)
+        done()
+      })
+  
+      it('should only return a success message', function(done){
+        assert.hasAllKeys(this.requestResult.body, ["message"], "body does not have the required keys")
+        assert.equal(this.requestResult.body.message, "success", "message returned is not success")
+        done()
+      })
+  
+      it('new user should be available on db', async function(){
+          const userFromDb = await db('users').select("*").where({ email: newUser.email }).first()
+          assert.isDefined(userFromDb, "new user not found on db")
       })
     })
 
-    it('should return status code 200', function(done){
-      assert.equal(this.requestResult.status, 200)
-      done()
+    describe('insert a new user with only an email', function(){
+      before(async function(){
+        await db('users').delete()
+        await this.requester.post('/auth/register').send({ email: newUser.email }).then((res, err) => {
+          this.requestResult = res
+        })
+      })
+  
+      it('should return status code 400', function(done){
+        assert.equal(this.requestResult.status, 400)
+        done()
+      })
     })
 
-    it('should only return a success message', function(done){
-      assert.hasAllKeys(this.requestResult.body, ["message"], "body does not have the required keys")
-      assert.equal(this.requestResult.body.message, "success", "message returned is not success")
-      done()
+    describe('insert a new user with only a password', function(){
+      before(async function(){
+        await db('users').delete()
+        await this.requester.post('/auth/register').send({ pass: newUser.pass }).then((res, err) => {
+          this.requestResult = res
+        })
+      })
+  
+      it('should return status code 400', function(done){
+        assert.equal(this.requestResult.status, 400)
+        done()
+      })
     })
 
-    it('new user should be available on db', async function(){
-        const userFromDb = await db('users').select("*").where({ email: newUser.email }).first()
-        assert.isDefined(userFromDb, "new user not found on db")
-        userFromDb
-    })
+
   })
 })
