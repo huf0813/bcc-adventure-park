5eb3ffba120e8d030270f3a87e7a427588bf606c
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-11 11:13:25 +0700

    Update database to add name and delete latest_token colum

--- src/services/auth.js
+++ src/services/auth.js
@@ -9,7 +9,7 @@ async function generateToken(userId){
   const token = (await crypto.randomBytes(48)).toString('base64')
   const userFromDb = await db('users').where({ id: userId }).select(["id", "email", "level"]).first()
   const expiresAt = Date.now() + TOKEN_LIFESPAN
-  session.set(token, { ...userFromDb }, TOKEN_LIFESPAN)
+  await session.set(token, { ...userFromDb }, TOKEN_LIFESPAN)
   return { token: token, expiresAt: expiresAt }
 }
 
