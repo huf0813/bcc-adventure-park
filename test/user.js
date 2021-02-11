dd4c63afdd95b15461999ce9cca791970dc78361
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-11 10:19:30 +0700

    Add test for register-and-request-token endpoint

--- test/user.js
+++ test/user.js
@@ -7,6 +7,13 @@ const db = require('../src/utils/db')
 
 const newUser = { email: "izf@izfaruqi.com", pass: "Tr0ub4dor&3" }
 
+function assertValidTokenObject(token){
+  assert.isObject(token, "token is not an object")
+  assert.hasAllKeys(token, ["token", "expiresAt"], "token doesn't have the required keys")
+  assert.isString(token.token)
+  assert.isNumber(token.expiresAt)
+}
+
 describe('/user endpoints', function(){
   describe('POST /user, for registering new users', function(){
     const url = "/user/register"
@@ -26,6 +33,32 @@ describe('/user endpoints', function(){
       it('should only return a success message', function(done){
         assert.hasAllKeys(this.requestResult.body, ["message"], "body does not have the required keys")
         assert.equal(this.requestResult.body.message, "success", "message returned is not success")
+        done()
+      })
+  
+      it('new user should be available in the db', async function(){
+          const userFromDb = await db('users').select("*").where({ email: newUser.email }).first()
+          assert.isDefined(userFromDb, "new user not found in the db")
+      })
+    })
+
+    describe('insert a new user and request token with an email and a password', function(){
+      before(async function(){
+        await db('users').delete()
+        await this.requester.post(url).send({ email: newUser.email, pass: newUser.pass, token: true }).then((res, err) => {
+          this.requestResult = res
+        })
+      })
+  
+      it('should return status code 200', function(done){
+        assert.equal(this.requestResult.status, 200)
+        done()
+      })
+  
+      it('should return a token and a success message', function(done){
+        assert.hasAllKeys(this.requestResult.body, ["message", "token"], "body does not have the required keys")
+        assert.equal(this.requestResult.body.message, "success", "message returned is not success")
+        assertValidTokenObject(this.requestResult.body.token)
         done()
       })
   
