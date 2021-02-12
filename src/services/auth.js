391a52b3d9454038139ac894e56b0de6eaca090f
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-12 10:28:10 +0700

    Change token generation and invalidation logic to accomodate the new token dispenser

--- src/services/auth.js
+++ src/services/auth.js
@@ -6,15 +6,18 @@ const TOKEN_LIFESPAN = 86400000
 
 async function generateToken(userId){
   const token = (await crypto.randomBytes(48)).toString('base64')
-  const userFromDb = await db('users').where({ id: userId }).select(["id", "email", "level"]).first()
+  const userFromDb = await db('users').where({ id: userId }).select(["email", "level"]).first()
   const expiresAt = Date.now() + TOKEN_LIFESPAN
-  await session.set(token, { ...userFromDb }, TOKEN_LIFESPAN)
+  await session.set(token, { id: userId, ...userFromDb }, TOKEN_LIFESPAN)
+  await session.set("token_" + userId, token, TOKEN_LIFESPAN)
   return { token: token, expiresAt: expiresAt }
 }
 
 async function invalidateToken(token){
   try {
+    const userId = (await session.get(token)).id
     await session.delete(token)
+    await session.delete("token_" + userId)
   } catch (e){
 
   } finally {
