012f266fcf6a753f8aa72f647ec041c835aab5be
Narukite <30924512+Narukite@users.noreply.github.com>
2021-02-12 23:51:58 +0700

    Model in progress

--- BCCAdventurePark/src/main/java/Main.java
+++ BCCAdventurePark/src/main/java/Main.java
@@ -1,21 +1,98 @@
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        System.out.println("Hello!");
         Model model = new Model();
+        Scanner scanner = new Scanner(System.in);
+        int userId;
 
-        String[][] parks = model.returnAllFromTable("parks");
-        for (String[] park : parks) {
-            for (String detail : park) {
-                System.out.println(detail);
+        boolean loopingMenu1 = true;
+        boolean loopingMenu2 = false;
+        while(loopingMenu1) {
+            System.out.println("--BCC Adventure Park--");
+            System.out.println("----Management App----");
+            System.out.println("| 1. Register as Visitor");
+            System.out.println("| 2. Login as Visitor");
+            System.out.println("| 3. Login as Admin");
+            System.out.println("| 4. Exit");
+            System.out.println();
+            System.out.print("I want to.. ");
+            int menuId = scanner.nextInt();
+            scanner.nextLine();
+
+            switch(menuId) {
+                case 1:
+                    System.out.println();
+                    System.out.println("Welcome, new visitor!");
+                    System.out.println("Please fill these informations correctly to register.");
+                    System.out.print("Your account's username: ");
+                    String username = scanner.nextLine();
+                    System.out.print("Your account's password: ");
+                    String password = scanner.nextLine();
+                    System.out.print("Your full name: ");
+                    String fullName = scanner.nextLine();
+                    boolean registered = model.insertVisitorIntoVisitors(
+                            username, password, fullName);
+                    if(registered) {
+                        System.out.println("Registration successful!");
+
+                        System.out.println("Your identification number is " +
+                                model.returnUserIdIfVisitorSuccessfullyLogin
+                                (username, password) + ".");
+                        System.out.println("Don't forget to login!");
+                    } else{
+                        System.out.println("Registration failed.");
+                    }
+                    System.out.println();
+                    break;
+                case 2:
+                    System.out.println();
+                    System.out.println("Please input your login credentials.");
+                    System.out.print("Username: ");
+                    username = scanner.nextLine();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine();
+                    userId = model.returnUserIdIfVisitorSuccessfullyLogin
+                            (username, password);
+                    if(userId != 0) {
+                        loopingMenu2 = true;
+                    } else {
+                        System.out.println("Your login credentials are false.");
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println();
+                    System.out.println("Please input your login credentials.");
+                    System.out.print("Username: ");
+                    username = scanner.nextLine();
+                    System.out.print("Password: ");
+                    password = scanner.nextLine();
+                    userId = model.returnUserIdIfAdminSuccessfullyLogin
+                            (username, password);
+                    if(userId != 0) {
+                        loopingMenu2 = true;
+                    } else {
+                        System.out.println("Your login credentials are false.");
+                        System.out.println();
+                    }
+                    break;
+                case 4:
+                    loopingMenu1 = false;
+                    break;
+                default:
+                    System.out.println("Please input a number.");
+                    System.out.println();
+                    break;
+
             }
-        }
 
-        String[] nameOfParks = model.returnColumnFromTable("name", "parks");
-        for (String name : nameOfParks) {
-            System.out.println(name);
-        }
+            while(loopingMenu2) {
+                System.out.println("Well, welcome!");
+                break;
+            }
 
+        }
     }
 }
--- BCCAdventurePark/src/main/java/Model.java
+++ BCCAdventurePark/src/main/java/Model.java
@@ -1,40 +1,40 @@
 import java.sql.*;
 
 public class Model {
-    private final Statement statement1;
-    private final Statement statement2;
+    private final Connection connection;
     private ResultSet resultSet1;
     private ResultSet resultSet2;
-    private String sql;
 
     public Model() throws SQLException {
-        Connection connection = DriverManager.getConnection(
+        connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/bccadventurepark",
                 "root",
                 "narukam1");
-        statement1 = connection.createStatement();
-        statement2 = connection.createStatement();
     }
 
-    public int returnRowCountFromTable(String tableName) throws SQLException {
-        sql = "SELECT COUNT(*) FROM " + tableName;
-        resultSet2 = statement2.executeQuery(sql);
+    private int returnRowCountFromTable(String tableName) throws SQLException {
+        String query = "SELECT COUNT(*) FROM ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, tableName);
+        resultSet2 = preparedStatement.executeQuery();
         resultSet2.next();
         return resultSet2.getInt(1);
     }
 
-    public int returnColumnCountFromTable(String tableName) throws SQLException {
-        sql = "SELECT COUNT(*) " +
+    private int returnColumnCountFromTable(String tableName) throws SQLException {
+        String query = "SELECT COUNT(*) " +
                 "FROM INFORMATION_SCHEMA.COLUMNS " +
                 "WHERE TABLE_SCHEMA = 'bccadventurepark' " +
-                "AND TABLE_NAME = '" + tableName + "'";
-        resultSet2 = statement2.executeQuery(sql);
+                "AND TABLE_NAME = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, tableName);
+        resultSet2 = preparedStatement.executeQuery();
         resultSet2.next();
         return resultSet2.getInt(1);
     }
 
-    public String[] resultSetTo1DArray(String tableName,
-                                       ResultSet resultSet) throws SQLException {
+    private String[] resultSetTo1DArray(String tableName,
+                                        ResultSet resultSet) throws SQLException {
         int rowCount = returnRowCountFromTable(tableName);
         String[] array = new String[rowCount];
 
@@ -46,8 +46,8 @@ public class Model {
         return array;
     }
 
-    public String[][] resultSetTo2DArray(String tableName,
-                                         ResultSet resultSet) throws SQLException {
+    private String[][] resultSetTo2DArray(String tableName,
+                                          ResultSet resultSet) throws SQLException {
         int rowCount = returnRowCountFromTable(tableName);
         int columnCount = returnColumnCountFromTable(tableName);
         String[][] array = new String[rowCount][columnCount];
@@ -66,16 +66,71 @@ public class Model {
         return array;
     }
 
+    public boolean insertVisitorIntoVisitors(String username,
+                                             String password,
+                                             String fullName) throws SQLException {
+        String query = "INSERT INTO visitors (username, password, full_name) " +
+                "VALUES (?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        preparedStatement.setString(3, fullName);
+        int rowCount = preparedStatement.executeUpdate();
+        return rowCount != 0;
+    }
+
+    public int returnUserIdIfVisitorSuccessfullyLogin(String username,
+                                                      String password) throws SQLException {
+        String query = "SELECT * FROM visitors WHERE username = ? AND password = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        resultSet1 = preparedStatement.executeQuery();
+        if(resultSet1.next()) {
+            return resultSet1.getInt("id");
+        } else return 0;
+    }
+
+    public int returnUserIdIfAdminSuccessfullyLogin(String username,
+                                                      String password) throws SQLException {
+        String query = "SELECT * FROM admins WHERE username = ? AND password = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+        resultSet1 = preparedStatement.executeQuery();
+        if(resultSet1.next()) {
+            return resultSet1.getInt("id");
+        } else return 0;
+    }
+
     public String[][] returnAllFromTable(String tableName) throws SQLException {
-        sql = "select * from " + tableName;
-        resultSet1 = statement1.executeQuery(sql);
+        String query = "SELECT * FROM ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, tableName);
+        resultSet1 = preparedStatement.executeQuery();
+
+        return resultSetTo2DArray(tableName, resultSet1);
+    }
+
+    public String[][] returnAllFromTableWhereColumnEqualsCondition(String tableName,
+                                                                   String columnName,
+                                                                   String condition) throws SQLException {
+        String query = "SELECT * FROM ? WHERE ? = ?" ;
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, tableName);
+        preparedStatement.setString(2, columnName);
+        preparedStatement.setString(3, condition);
+        resultSet1 = preparedStatement.executeQuery();
 
         return resultSetTo2DArray(tableName, resultSet1);
     }
 
     public String[] returnColumnFromTable(String columnName, String tableName) throws SQLException {
-        sql = "select " + columnName + " from " + tableName;
-        resultSet1 = statement1.executeQuery(sql);
+        String query = "SELECT ? FROM ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, columnName);
+        preparedStatement.setString(2, tableName);
+        resultSet1 = preparedStatement.executeQuery();
 
         return resultSetTo1DArray(tableName, resultSet1);
     }
