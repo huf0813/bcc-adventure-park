0a180eb1556d0e5fbbb24f498642427f15e5c710
Ibrahim Faruqi <izf@izfaruqi.com>
2021-02-12 18:01:57 +0700

    Remove prefix on root endpoint

--- src/routes/registerRoutes.js
+++ src/routes/registerRoutes.js
@@ -1,5 +1,5 @@
 module.exports = async (fastify, opts) => {
-  fastify.register(require('./root'), { prefix: '/'})
+  fastify.register(require('./root'))
   fastify.register(require('./park'), { prefix: '/park'})
   fastify.register(require('./user'), { prefix: '/user'})
 }
